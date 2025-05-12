94a30add882f05c2d383de6c1d6afd5877957b10
a-ramyaarul <ramyaarul812@gmail.com>
2025-05-12 18:12:09 +0530

    commit

--- src/main/lab.py
+++ src/main/lab.py
@@ -4,25 +4,30 @@ as well as creating a table, inserting data, and selecting that data.
 """
 import sqlite3
 
-
-conn = "TODO: Create a database connection"
-cursor = "TODO: create a cursor with the connection"
+conn = sqlite3.connect('dogs.db')
+cursor = conn.cursor()
 
 
 # Create a dogs table with autoincrementing ID
 def create_dogs_table():
-
-    """TODO"""
-
+    cursor.execute('''
+            CREATE TABLE IF NOT EXISTS dogs (
+                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                name TEXT not null,
+                breed TEXT not null,
+                age INTEGER
+            )
+        ''')
+        
+    
 
 # TODO: Complete insert_dog() by inserting a new dog (provided in the parameters) into the "dogs" table.
 def insert_dog(name, breed, age):
-
-    """TODO"""
-
+     cursor.execute('INSERT INTO dogs (name, breed, age) VALUES (?, ?, ?)', (name, breed, age))
+     conn.commit()
 
 # TODO: Complete select_all_dogs() by selecting all rows from the "dogs" table *and returning them*.
 def select_all_dogs():
-
-    # return the rows
-    return """TODO"""
+    cursor.execute('SELECT * FROM dogs')
+    rows = cursor.fetchall()
+    return rows
